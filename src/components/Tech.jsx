f8d3f92c34ad6a52064b8b051cfd4e6b82626f94
Rohit Jangra <rohit19444@iiitd.ac.in>
2025-07-18 10:47:22 +0530

    Added skills name too

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -8,8 +8,13 @@ const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+        <div className='w-28 h-28 flex flex-col items-center' key={technology.name}>
+          <div className='w-28 h-28 mb-2'>
+            <BallCanvas icon={technology.icon} />
+          </div>
+          <p className='text-secondary text-[14px] font-medium text-center leading-tight'>
+            {technology.name}
+          </p>
         </div>
       ))}
     </div>
